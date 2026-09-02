2084099f30ac76d17f723384fa055b3631e966f7
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-09-01 23:03:54 -0400

    feat: add function for tab panels

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import './style.css'
+
+
+const tabButtons = document.querySelectorAll('.tab-btn')
+const panels = document.querySelectorAll('[data-panel]')
+
+tabButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const target = btn.dataset.tab;
+
+    panels.forEach(panel => {
+      panel.classList.toggle('hidden', panel.dataset.panel !== target)
+      panel.classList.toggle('flex', panel.dataset.panel === target)
+    })
+    tabButtons.forEach(b => {
+      const active = b === btn
+      if (active) {
+        b.classList.add('border-b', 'border-b-red-500', 'text-red-500');
+        b.classList.remove('border-none');
+      } else {
+        b.classList.remove('border-b', 'border-b-red-500', 'text-red-500');
+        b.classList.add('border-none');
+      }
+      b.setAttribute('aria-selected', active)
+
+    })
+  })
+})
